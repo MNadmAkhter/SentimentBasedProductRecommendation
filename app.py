e09ad81fa057c5c35b35cc1e604e2c201b7a0c5b
MNadmAkhter <111018817+MNadmAkhter@users.noreply.github.com>
2023-08-18 18:16:44 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ def prediction():
         print(f"retrieving items....{len(items)}")
         print(items)
         # data=[items.to_html(classes="table-striped table-hover", header="true",index=False)
+       
         return render_template("index.html", column_names=items.columns.values, row_data=list(items.values.tolist()), zip=zip)
     else:
         return render_template("index.html", message="User Name doesn't exists, No product recommendations at this point of time!")
